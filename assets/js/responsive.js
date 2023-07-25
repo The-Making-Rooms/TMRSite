497830a9388533e7e0105cdbb34edbc7d28175c6
Uzair Patel <36827239+DottiestDuke47@users.noreply.github.com>
2023-07-25 11:57:27 +0100

    Added Pages upstream

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -4,20 +4,20 @@ window.onscroll = function() {myFunction()};
 // Get the navbar
 var navbar = document.getElementById("navbar-responsive");
 var image = document.getElementById("header-image");
-
+var navlist = document.getElementById("navul");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky && navbar.className != "navbar responsive") {
-    image.src = "assets/img/making-rooms-logo-scroll.png";
+  if (window.pageYOffset >= sticky && navbar.classList.contains("w-full")) {
+    image.src = "/assets/img/making-rooms-logo-scroll.png";
     image.classList.remove("h-28");
     image.classList.add("h-16");
     navbar.classList.remove("top-12");
-  } else {
-    image.src = "assets/img/making-rooms-logo.png";
+  } else if (window.pageYOffset < sticky && navbar.classList.contains("w-full")){
+    image.src = "/assets/img/making-rooms-logo.png";
     image.classList.remove("h-16");
     image.classList.add("h-28");
     navbar.classList.add("top-12");
@@ -25,13 +25,14 @@ function myFunction() {
 }
 
 function responsiveNavbar() {
-  if (navbar.className === "navbar" || navbar.className === "navbar sticky") {
-    navbar.classList.remove("sticky");
-    navbar.classList.add("responsive");
-    
+  if (navbar.classList.contains('flex-row')) {
+    navbar.classList.remove("top-12", "w-full", "flex-row", "justify-between");
+    navlist.classList.remove("[&>*:not(:first-child)]:hidden");
+    navbar.classList.add("text-center", "justify-center", "w-screen", "h-screen", "flex-col", "p-4");
+
   } else {
-    navbar.classList.add("sticky")
-    navbar.classList.remove("responsive");
-    
+    navbar.classList.add("top-12", "w-full", "flex-row", "justify-between");
+    navlist.classList.add("[&>*:not(:first-child)]:hidden");
+    navbar.classList.remove("text-center", "justify-center", "w-screen", "h-screen", "flex-col", "p-4");
   }
 }
